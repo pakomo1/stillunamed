bef47ac7556897de83a95facfa3dcb71fd30d86a
pakomo1 <iampakomo@gmail.com>
2022-10-02 15:27:16 +0300

    Added script that reads files

--- Assets/Scripts/TextEditroScripts/Writing Field/ReadFiles.cs
+++ Assets/Scripts/TextEditroScripts/Writing Field/ReadFiles.cs
@@ -1,13 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using TMPro;
+using System.IO;
 
 public class ReadFiles : MonoBehaviour
 {
+
+    [SerializeField] private TMP_InputField inputfield;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //should take the path that has been selected form the Open Folder or the dir
+        //for now i am just going to use a static file form a static dir
+
+          string[] files = Directory.GetFiles(@"D:\");
+
+
+
+        List<string> lines = File.ReadAllLines(@"C:\Users\Maixm\Documents\file.txt").ToList();
+        foreach (string line in lines)
+        {
+            inputfield.text+= line + "\n";
+        }
+
     }
 
     // Update is called once per frame
--- Assets/Scripts/TextEditroScripts/Writing Field/UpdateFIles.cs
+++ Assets/Scripts/TextEditroScripts/Writing Field/UpdateFIles.cs
@@ -1,23 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
 
-public class DirScript : OpenFolder
+public class UpdateFIles : MonoBehaviour
 {
-    private string path1;
-    [SerializeField] private TextMeshProUGUI Text;
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.path1 = base.path;  
-        Text.text = path1;
+        
     }
 }
