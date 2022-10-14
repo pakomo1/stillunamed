70721c6a38087e20a3b1efa78029154cad51b5d7
hristo-darlyanov <memeda963@gmail.com>
2022-10-14 16:12:40 +0300

    Oauth2 First commit
    Oauth2 implemented in game with working backend.

---
+++ New Project/Assets/Scripts/GitHub/Oauth2/Oauth2AccessToken.cs
@@ -0,0 +1,64 @@
+using Mono.Cecil.Cil;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+using Octokit;
+using Cysharp.Threading.Tasks;
+
+public class Oauth2AccessToken : MonoBehaviour
+{
+    private string code;
+    private string clientId = "086490aaf0f9ef0b33e4";
+    private string clientSecret = "f1fe8180ea2712c9ce2a282a035799e9f2129093";
+    private GitHubClient client = new GitHubClient(new ProductHeaderValue("TestApp"));
+    private string result;
+    private string[] prefixes = new string[1] { "http://localhost:3000/callback/" };
+
+    public async void AuthorizeBegin()
+    {
+        HttpListener listener = new HttpListener();
+        foreach (string s in prefixes)
+        {       
+            listener.Prefixes.Add(s);
+        }
+        listener.Start();
+        print("Listening...");
+
+        // Note: The GetContext method blocks while waiting for a request.
+        HttpListenerContext context = listener.GetContext();
+        HttpListenerRequest request = context.Request;
+
+        // Obtain a response object.
+        HttpListenerResponse response = context.Response;
+
+        // Construct a response.
+        string responseString = "<HTML><BODY>DONE!</BODY></HTML>";
+        byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+
+        // Get a response stream and write the response to it.
+        response.ContentLength64 = buffer.Length;
+        System.IO.Stream output = response.OutputStream;
+        output.Write(buffer, 0, buffer.Length);
+
+        // Close the output stream.
+        output.Close();
+        listener.Stop();
+
+        // Get code from URI
+        string[] rawUri = request.RawUrl.Split('=');
+        code = rawUri[1];
+        print(code);
+
+        // Get Access Token with code
+        result = await Authorize();
+        print(result);
+    }
+    public async UniTask<string> Authorize()
+    {
+        var request = new OauthTokenRequest(clientId, clientSecret, code);
+        var token = await client.Oauth.CreateAccessToken(request);
+        return token.AccessToken;
+    }
+}
