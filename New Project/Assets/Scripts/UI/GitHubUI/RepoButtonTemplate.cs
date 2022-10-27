477e74445cdcae14911d314f38408c1ae18a514e
pakomo1 <iampakomo@gmail.com>
2022-10-28 00:13:29 +0300

    Updated WWW to unityWebRequest

--- New Project/Assets/Scripts/UI/GitHubUI/RepoButtonTemplate.cs
+++ New Project/Assets/Scripts/UI/GitHubUI/RepoButtonTemplate.cs
@@ -1,27 +1,18 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Networking;
+using System.Collections;
 
 public class RepoButtonTemplate : MonoBehaviour
 {
     [SerializeField] private GameObject buttonTemplate;
-
+    [SerializeField] private GameObject mainPanel;
 
     public void CreateButton(string text, string description, string profilePicUrl, bool visibility)
     {
-        var button = Instantiate(buttonTemplate, transform);
-        button.SetActive(true);
-        button.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = text;
-        button.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = description;
-        //Sprite image = GetImage(profilePicUrl);
-        //button.transform.GetChild(2).GetChild(0).GetComponent<Image>().sprite = image;
-        GetImage(profilePicUrl, button.transform.GetChild(2).GetChild(0).GetComponent<Image>().sprite);
-        if (!visibility)
+        if (mainPanel.activeSelf)
         {
             var button = Instantiate(buttonTemplate, transform);
             button.SetActive(true);
@@ -43,27 +34,16 @@ public class RepoButtonTemplate : MonoBehaviour
         }
     }
 
-     private IEnumerator GetImage(string url, Sprite img)
+    private IEnumerator GetImage(string url, Image img)
     {
-        UnityWebRequest request = UnityWebRequestTexture.GetTexture(url);
-        yield return request.SendWebRequest();
 
-        if (request.isNetworkError || request.isHttpError)
-        {
-            print(request.error);
-        }
-        else
-        {
-            Texture2D myTexture = ((DownloadHandlerTexture)request.downloadHandler).texture;
-            Sprite image = Sprite.Create(myTexture, new Rect(0, 0, myTexture.width, myTexture.height), new Vector2(0, 0));
-            img = image;
-        }
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture(url);
 
+        yield return www.SendWebRequest();
 
-        /*WWW www = new WWW(url);
-         while (!www.isDone)
+        if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
         {
-            print("Error: "+www.error);
+            print("Error: " + www.error);
         }
         else
         {
@@ -79,6 +59,4 @@ public class RepoButtonTemplate : MonoBehaviour
          }
          return Sprite.Create(www.texture, new Rect(0, 0, www.texture.width, www.texture.height), new Vector2(0, 0));*/
     }
-
-
 }
