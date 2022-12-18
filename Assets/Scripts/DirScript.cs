3d84c56f741f2b47735459671c8fe9bf3544fea7
pakomo1 <83059895+pakomo1@users.noreply.github.com>
2022-12-18 04:34:34 -0800

    created on mouse over dir text
    when the mouse is over the directory text now the hole directory is displayed

--- Assets/Scripts/DirScript.cs
+++ Assets/Scripts/DirScript.cs
@@ -1,26 +1,41 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.EventSystems;
 
-public class DirScript : MonoBehaviour
+public class DirScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    [SerializeField] private OpenFolder openFolder; 
+    [SerializeField] private OpenFolder openFolder;
     [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private GameObject holeCurrentDir;
+
+    private TextMeshProUGUI holeCurrentDirText;
     // Start is called before the first frame update
     private string path;
     void Start()
     {
-       
+        holeCurrentDirText = holeCurrentDir.GetComponent<TextMeshProUGUI>();  
     }
-    
+
     // Update is called once per frame
     void Update()
     {
         path = openFolder.path;
-
+        holeCurrentDirText.text = path;
         if (path.Length > 24)
         {
             string newText = path.Substring(24);
             text.text = newText + "...";
         }
+
+    }
+  
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        holeCurrentDir.SetActive(true);   
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        holeCurrentDir.SetActive(false);
     }
 }
