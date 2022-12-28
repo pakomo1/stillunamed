a5e6915853ffd03e390f562966dc0f813323d703
pakomo1 <iampakomo@gmail.com>
2022-12-28 15:59:59 +0200

    back to www library
    Replaced UnityWebRequset with the WWW library

--- New Project/Assets/Scripts/UI/GitHubUI/RepoButtonTemplate.cs
+++ New Project/Assets/Scripts/UI/GitHubUI/RepoButtonTemplate.cs
@@ -1,23 +1,22 @@
-using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.Networking;
-using System.Collections;
 
 public class RepoButtonTemplate : MonoBehaviour
 {
     [SerializeField] private GameObject buttonTemplate;
 
-    public void CreateButton(string text, string description, string profilePicUrl, bool visibility)
+    public async void CreateButton(string text, string description, string profilePicUrl, bool visibility)
     {
         var button = Instantiate(buttonTemplate, transform);
-
         button.SetActive(true);
         button.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = text;
         button.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = description;
-
-        StartCoroutine(GetImage(profilePicUrl, button.transform.GetChild(2).GetChild(0).GetComponent<Image>()));
+        Sprite image = await GetImage(profilePicUrl);
+        button.transform.GetChild(2).GetChild(0).GetComponent<Image>().sprite = image;
         if (!visibility)
         {
             button.transform.GetChild(3).GetComponent<Image>().color = Color.green;
@@ -28,26 +27,13 @@ public class RepoButtonTemplate : MonoBehaviour
         }
     }
 
-    private IEnumerator GetImage(string url, Image img)
+    private async Task<Sprite> GetImage(string url)
     {
-        UnityWebRequest www = UnityWebRequestTexture.GetTexture(url);
-        yield return www.SendWebRequest();
-        if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
+        WWW www = new WWW(url);
+        while (!www.isDone)
         {
-            print("Error: " + www.error);
+            await Task.Yield();
         }
-        else
-        {
-            DownloadHandlerTexture textureDownloadHandler = (DownloadHandlerTexture)www.downloadHandler;
-            Texture2D texture = textureDownloadHandler.texture;
-            Sprite image = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0, 0));
-            img.sprite = image;
-        }
-        /*WWW www = new WWW(url);
-         while (!www.isDone)
-         {
-             await Task.Yield();
-         }
-         return Sprite.Create(www.texture, new Rect(0, 0, www.texture.width, www.texture.height), new Vector2(0, 0));*/
+        return Sprite.Create(www.texture, new Rect(0, 0, www.texture.width, www.texture.height), new Vector2(0, 0));
     }
 }
