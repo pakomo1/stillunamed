07fe25792cc178d79e90b6e1568425f605f33284
pakomo1 <iampakomo@gmail.com>
2023-08-30 12:58:00 +0300

    created door lobby explorer
    It is simply a door in the scene that when the player is close enough will be able to press "e" and then the lobby explorer will open(not yet because it is not created)

---
+++ New Project/Assets/Scripts/Environment/Room1/OpenLobbyExplorer.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLobbyExplorer : MonoBehaviour
+{
+    private bool triggerActive;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            triggerActive = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            triggerActive = false;  
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(triggerActive && Input.GetKeyDown(KeyCode.E))
+        {
+            print("We now open the lobby explorer");
+        }
+    }
+}
