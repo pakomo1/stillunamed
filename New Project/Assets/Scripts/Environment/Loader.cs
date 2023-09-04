1c99029225daf992f220b237173023f85c726827
pakomo1 <iampakomo@gmail.com>
2023-09-04 15:03:24 +0300

    created scene loader

---
+++ New Project/Assets/Scripts/Environment/Loader.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loader : MonoBehaviour
+{
+   public enum Scene
+    {
+        Scene,
+        GameScene
+    }
+
+    private static Scene targetScene;
+
+    public static void Load(Scene targetscene)
+    {
+        Loader.targetScene = targetscene;
+        //SceneManager.LoadScene();
+    }
+    public static void LoadNetwrok(Scene targetScene)
+    {
+        NetworkManager.Singleton.SceneManager.LoadScene(targetScene.ToString(), LoadSceneMode.Single);
+    }
+}
