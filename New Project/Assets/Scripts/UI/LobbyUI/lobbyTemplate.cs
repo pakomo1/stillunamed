db0a53295b880bb60d77f705be9439de4c479703
pakomo1 <iampakomo@gmail.com>
2023-09-27 18:21:30 +0300

    created a lobby template

--- New Project/Assets/Scripts/UI/LobbyUI/lobbyTemplate.cs
+++ New Project/Assets/Scripts/UI/LobbyUI/lobbyTemplate.cs
@@ -2,16 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class lobbyTemplate : MonoBehaviour
 {
     [SerializeField] private GameObject buttonTemplate;
    public void GenerateLobbie(string lobbyName, string joinedAndMaxPlayersCount)
    {
-        var button = Instantiate(buttonTemplate, transform);
-        button.SetActive(true);
+        GameObject lobbyGameObject = Instantiate(buttonTemplate, transform);
+        lobbyGameObject.SetActive(true);
 
-        button.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = lobbyName;
-        button.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = joinedAndMaxPlayersCount;
+        lobbyGameObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = lobbyName;
+        lobbyGameObject.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = joinedAndMaxPlayersCount;
+
+        Button lobbuButton = lobbyGameObject.GetComponent<Button>();
+        lobbuButton.onClick.AddListener(OnClickLobbyButton);
+
+    }
+
+    private void OnClickLobbyButton()
+    {
+
     }
 }
