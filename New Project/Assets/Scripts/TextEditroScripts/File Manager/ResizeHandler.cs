91096ce446ea168e7ccbf4102345c0cde99ca7da
pakomo1 <iampakomo@gmail.com>
2024-03-23 00:10:44 +0200

    remade dragger

--- New Project/Assets/Scripts/TextEditroScripts/File Manager/ResizeHandler.cs
+++ New Project/Assets/Scripts/TextEditroScripts/File Manager/ResizeHandler.cs
@@ -2,24 +2,34 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ResizeHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+public class ResizeHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     private float offset;
     private bool isResizing = false;
-    private LayoutElement layoutElement;
+    private RectTransform fileManagerRectTransform;
+
+    [SerializeField]
+    private RectTransform textAreaRectTransform; // Serialized field for the RectTransform of the text editor area
+
+    [SerializeField]
+    private float minWidth = 100f; // Minimum width of the FileManager
+
+    [SerializeField]
+    private float maxWidth = 500f; // Maximum width of the FileManager
 
     private void Start()
     {
-        layoutElement = transform.parent.GetComponent<LayoutElement>();
+        // Get the RectTransform from the parent (FileManager)
+        fileManagerRectTransform = transform.parent.GetComponent<RectTransform>();
     }
 
-    public void OnPointerDown(PointerEventData eventData)
+    public void OnBeginDrag(PointerEventData eventData)
     {
         isResizing = true;
-        offset = layoutElement.preferredWidth - eventData.position.x;
+        offset = fileManagerRectTransform.rect.width - eventData.position.x;
     }
 
-    public void OnPointerUp(PointerEventData eventData)
+    public void OnEndDrag(PointerEventData eventData)
     {
         isResizing = false;
     }
@@ -30,7 +40,14 @@ public class ResizeHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandl
         {
             float mouseX = eventData.position.x;
             float newWidth = mouseX + offset;
-            layoutElement.preferredWidth = newWidth;
+
+            // Clamp the new width to the minimum and maximum values
+            newWidth = Mathf.Clamp(newWidth, minWidth, maxWidth);
+
+            fileManagerRectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, newWidth);
+
+            // Adjust the left padding of the Text Area based on the width of the FileManager
+            textAreaRectTransform.offsetMin = new Vector2(newWidth, textAreaRectTransform.offsetMin.y);
         }
     }
 }
